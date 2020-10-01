74c831e8495d94a72f347a6d94a1eb303939d344
Abelard Bautista <abelardbautista@gmail.com>
2020-10-01 00:00:04 -0700

    Ask user name, save value in variable, print text.

--- w1/hello/hello.c
+++ w1/hello/hello.c
@@ -1,6 +1,11 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    // Ask user what their name is and save value of string in variable 'name'
+    string name = get_string("What is your name?\n");
+
+    // Print formatted text with variable 'name'
+    printf("hello, %s\n", name);
 }
