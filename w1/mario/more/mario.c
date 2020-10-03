b92cf04820500082e7d2fcff69bf0ebc60262184
Abelard Bautista <abelardbautista@gmail.com>
2020-10-02 22:23:02 -0700

    Added comments. Print right pyramid.

--- w1/mario/more/mario.c
+++ w1/mario/more/mario.c
@@ -9,14 +9,23 @@ int main(void)
         n = get_int("Height: ");
     }
 
+    // Limit input to be between 1 and 8
     while (n < 1 || n > 8);
+    // Print Row
     for (int i = 0; i < n; i++)
     {
+        // Print Left Column Spacer: "."
         for (int j = n - 1; j > i; j--)
         {
             printf(" ");
         }
+        // Print Left Pyramid: "#"
         for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        // Print Right Pyramid: "#"
+        for (int l = 0; l < i + 1; l++)
         {
             printf("#");
         }
