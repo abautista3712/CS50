33e6cf44da1236a92528635fdb8b54855a539a9b
Abelard Bautista <abelardbautista@gmail.com>
2020-10-05 23:46:04 -0700

    If cents exactly divisible by 25, print num of quarters.

--- w1/greedyAlgorithms/cash/cash.c
+++ w1/greedyAlgorithms/cash/cash.c
@@ -5,6 +5,8 @@
 int main(void)
 {
     float change;
+    int cents;
+    int numCoins = 0;
     do
     {
         change = get_float("Input total change owed: ");
@@ -12,7 +14,21 @@ int main(void)
 
     while (change <= 0);
     {
-        int cents = round(change * 100);
-        printf("%i\n", cents);
+        cents = round(change * 100);
+        int quarters = cents / 25;
+        if (cents % 25 == 0)
+        {
+            numCoins = quarters;
+        }
+        else
+        {
+            for (int i = 0; i < quarters; i++)
+            {
+                // cents = cents - 25;
+                cents = cents - (25 * i);
+                numCoins = cents;
+            }
+        }
+        printf("%i\n", numCoins);
     }
 }
