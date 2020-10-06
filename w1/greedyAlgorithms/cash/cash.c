7683edb2ed2a6cf880319fb4861f299c9a3a8921
Abelard Bautista <abelardbautista@gmail.com>
2020-10-06 01:39:22 -0700

    Formatting. Added comments.

--- w1/greedyAlgorithms/cash/cash.c
+++ w1/greedyAlgorithms/cash/cash.c
@@ -4,35 +4,44 @@
 
 int main(void)
 {
+    // Set variables 'change', 'cents', and 'numCoins'
     float change;
     int cents;
     int numCoins = 0;
+
+    // Ask for user input
     do
     {
         change = get_float("Input total change owed: ");
     }
 
+    // Continue to ask for user input until number is > 0
     while (change <= 0);
     {
+        // Convert dollars to cents by multiplying change by 100 and round number using math.h library
         cents = round(change * 100);
+        // Handle quarters: divide cents by 25. Each loop will add 1 to numCoins and subtract 25 from cents to produce balance.
         int quarters = cents / 25;
         for (int i = 0; i < quarters; i++)
         {
-            cents = cents - 25;
             numCoins++;
+            cents = cents - 25;
         }
+        // Handle dimes: divide remaining cents by 10. Each loop will add 1 to numCoins and subtract 10 to produce balance.
         int dimes = cents / 10;
         for (int j = 0; j < dimes; j++)
         {
-            cents = cents - 10;
             numCoins++;
+            cents = cents - 10;
         }
+        // Handle nickels: divide remaining cents by 5. Each loop will add 1 to numCoins and subtract 5 to produce balance.
         int nickels = cents / 5;
         for (int k = 0; k < nickels; k++)
         {
-            cents = cents - 5;
             numCoins++;
+            cents = cents - 5;
         }
+        // Handle pennies: remaining cents are added to numCoins.
         numCoins = numCoins + cents;
         printf("%i\n", numCoins);
     }
