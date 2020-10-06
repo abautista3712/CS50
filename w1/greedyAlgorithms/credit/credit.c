b6df1daa44aa42d7c3c890f2cbd9a1548c8c534f
Abelard Bautista <abelardbautista@gmail.com>
2020-10-06 03:29:37 -0700

    Added comments.

--- w1/greedyAlgorithms/credit/credit.c
+++ w1/greedyAlgorithms/credit/credit.c
@@ -20,22 +20,31 @@ int main(void)
             digitsNum = digitsNum / 10;
             countDigits++;
         }
+        // Cards with 13 digits are VISA
         if (countDigits == 13)
         {
             printf("VISA\n");
         }
+        // Cards with 15 digits are VISA
         else if (countDigits == 15)
         {
             printf("AMEX\n");
         }
+        // Cards with 16 digits are either VISA or Mastercard
         else if (countDigits == 16)
         {
+            // Cards starting with 4 as the first digit are VISA
             if (floor(creditNum / (1 * pow(10, 15))) == 4)
             {
                 printf("VISA\n");
             }
             else
                 (printf("MASTERCARD\n"));
+        }
+        // Any other combination of numbers are invalid
+        else
+        {
+            printf("INVALID\n")
         }
         printf("%i\n", countDigits);
     }
