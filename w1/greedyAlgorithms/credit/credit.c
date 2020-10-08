363a7754492059d314a5af84815a125ad98117f7
Abelard Bautista <abelardbautista@gmail.com>
2020-10-07 20:31:47 -0700

    Added luhnCheck function which prints last value of input.

--- w1/greedyAlgorithms/credit/credit.c
+++ w1/greedyAlgorithms/credit/credit.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <math.h>
 
-void test(void);
+void luhnCheck(long ccNum);
 
 int main(void)
 {
@@ -26,6 +26,7 @@ int main(void)
         if (countDigits == 13)
         {
             printf("VISA\n");
+            luhnCheck(creditNum);
         }
         // Cards with 15 digits are VISA
         else if (countDigits == 15)
@@ -48,72 +49,13 @@ int main(void)
         {
             printf("INVALID\n");
         }
-        printf("%i\n", countDigits);
-        test();
-    }
-#include <stdio.h>
-#include <cs50.h>
-#include <math.h>
-
-void check(long ccNum);
-
-    int main(void)
-    {
-        long creditNum;
-        do
-        {
-            creditNum = get_long("Input CC number to see if valid: ");
-        }
-
-        while (creditNum <= 0);
-        {
-            // Check number of digits
-            int countDigits = 0;
-            long digitsNum = creditNum;
-            while (digitsNum != 0)
-            {
-                digitsNum = digitsNum / 10;
-                countDigits++;
-            }
-            // Cards with 13 digits are VISA
-            if (countDigits == 13)
-            {
-                check(creditNum);
-                printf("VISA\n");
-            }
-            // Cards with 15 digits are VISA
-            else if (countDigits == 15)
-            {
-                printf("AMEX\n");
-            }
-            // Cards with 16 digits are either VISA or Mastercard
-            else if (countDigits == 16)
-            {
-                // Cards starting with 4 as the first digit are VISA
-                if (floor(creditNum / (1 * pow(10, 15))) == 4)
-                {
-                    printf("VISA\n");
-                }
-                else
-                    (printf("MASTERCARD\n"));
-            }
-            // Any other combination of numbers are invalid
-            else
-            {
-                printf("INVALID\n");
-            }
-            printf("%i\n", countDigits);
-            // test();
-        }
-    }
-
-    void check(long ccNum)
-    {
-        printf("%li\n", (ccNum * 0));
+        // printf("%i\n", countDigits);
+        // test();
     }
 }
 
-void test(void)
+void luhnCheck(long ccNum)
 {
-    printf("test\n");
+    int digitValue = ccNum % 10;
+    printf("%i\n", digitValue);
 }
