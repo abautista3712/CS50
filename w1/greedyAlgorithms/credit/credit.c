e398f1e86e2426844e5b555ed209f80943965a82
Abelard Bautista <abelardbautista@gmail.com>
2020-10-11 00:25:03 -0700

    Added more comments.

--- w1/greedyAlgorithms/credit/credit.c
+++ w1/greedyAlgorithms/credit/credit.c
@@ -9,6 +9,7 @@ void luhnCheck(long ccNum, int counterDigits);
 int main(void)
 {
     long creditNum;
+    // Ask for input until satisfies 'while' condition
     do
     {
         creditNum = get_long("Input CC number to see if valid: ");
@@ -18,7 +19,7 @@ int main(void)
     {
         // Check number of digits
         int counterDigits = countDigits(creditNum);
-        // Cards with 13, 15, 16 digits are VALID
+        // Cards with 13, 15, 16 digits are VALID and will undergo further verification
         if (counterDigits == 13 | counterDigits == 15 | counterDigits == 16)
         {
             verifyCC(creditNum, counterDigits);
@@ -33,11 +34,15 @@ int main(void)
     }
 }
 
+// Function to count credit card digits
 int countDigits(long numberToCount)
 {
+    // Set counter to 0
     int counterDigits = 0;
+    // While credit card has more than one digit to check, run the loop
     while (numberToCount != 0)
     {
+        // Each loop divides the credit card number by 10 and adds 1 to the counter
         numberToCount = numberToCount / 10;
         counterDigits++;
     }
@@ -46,26 +51,31 @@ int countDigits(long numberToCount)
 
 void verifyCC(long ccNum, int ccNumDigits)
 {
-    // AmEx Check First Two Numbers
+    // AMEX Verification: First Two Numbers
     int firstTwoCheck = floor(ccNum / (1 * pow(10, ccNumDigits - 2)));
     if (firstTwoCheck == 34 | firstTwoCheck == 37)
     {
+        // Luhn CheckSum
         luhnCheck(ccNum, ccNumDigits);
         printf("AMEX\n");
     }
-    //  MasterCard Check First Two Numbers
+    // MASTERCARD Verification: First Two Numbers
     else if (firstTwoCheck == 51 | firstTwoCheck == 52 | firstTwoCheck == 53 | firstTwoCheck == 54 | firstTwoCheck == 55)
     {
+        // Luhn CheckSum
         luhnCheck(ccNum, ccNumDigits);
         printf("MASTERCARD\n");
     }
+    // VISA: First Number
     else if (floor(ccNum / (1 * pow(10, ccNumDigits - 1))) == 4)
     {
+        // Luhn CheckSum
         luhnCheck(ccNum, ccNumDigits);
         printf("VISA\n");
     }
 }
 
+// CheckSum Algorithm
 void luhnCheck(long ccNum, int ccNumDigits)
 {
     bool x = false;
@@ -74,6 +84,7 @@ void luhnCheck(long ccNum, int ccNumDigits)
     int finalSum = 0;
     for (int i = 0; i < ccNumDigits; i++)
     {
+        // Calulate checkSum1
         if (x == true)
         {
             x = false;
@@ -97,6 +108,7 @@ void luhnCheck(long ccNum, int ccNumDigits)
                 }
             }
         }
+        // Calculate checkSum2
         else
         {
             x = true;
@@ -106,6 +118,7 @@ void luhnCheck(long ccNum, int ccNumDigits)
             checkSum2 = checkSum2 + digitValue;
         }
     }
+    // finalSum Check
     finalSum = checkSum1 + checkSum2;
     int digitValue = ccNum % 10;
     ccNum = ccNum / 10;
