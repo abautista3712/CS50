c2e8a7d4bbf2568ef32451c3b9525c91d9ba0dd7
Abelard Bautista <abelardbautista@gmail.com>
2020-10-12 04:38:03 -0700

    Accurately counts alphanumeric characters.

--- w2/readability/readability.c
+++ w2/readability/readability.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 void countLetters(string inputText);
 
 int main(void)
 {
-
     string inputText = get_string("Text: ");
     countLetters(inputText);
 }
 
 void countLetters(string inputText)
 {
-    int characters = 0;
+    int letters = 0;
     for (int i = 0, n = strlen(inputText); i < n; i++)
     {
-        characters++;
+        if (isalpha(inputText[i]) | isdigit(inputText[i]))
+        {
+            letters++;
+        }
     }
-    printf("%i\n", characters);
+    printf("%i\n", letters);
 }
