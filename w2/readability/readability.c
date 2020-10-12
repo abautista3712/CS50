599697fbec649f4c275a2b88005c0a6b23a3d9ed
Abelard Bautista <abelardbautista@gmail.com>
2020-10-12 04:15:41 -0700

    Count number of characters.

--- w2/readability/readability.c
+++ w2/readability/readability.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
+    int characters = 0;
     string inputText = get_string("Text: ");
-    printf("%s\n", input);
+    for (int i = 0, n = strlen(inputText); i < n; i++)
+    {
+        characters++;
+    }
+    printf("%i\n", characters);
 }
