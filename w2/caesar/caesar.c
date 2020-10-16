6cfba62deebbf1883c10dad0d3f50b48594bbfda
Abelard Bautista <abelardbautista@gmail.com>
2020-10-16 00:08:06 -0700

    Consolidated uppercase and lowercase functions.

--- w2/caesar/caesar.c
+++ w2/caesar/caesar.c
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void handleUppercase(int key, string strPlaintext, int j);
-void handleLowercase(int key, string strPlaintext, int j);
+void handleCase(int lowerbound, int upperbound, int key, string strPlaintext, int j);
+// void handleUppercase(int key, string strPlaintext, int j);
+// void handleLowercase(int key, string strPlaintext, int j);
 
 int main(int argc, string argv[])
 {
@@ -29,49 +30,31 @@ int main(int argc, string argv[])
     // Ask for user input for plaintext
     string strPlaintext = get_string("plaintext: ");
     // Check each character from the beginning to the end of the string length
+    printf("ciphertext: ");
     for (int j = 0, m = strlen(strPlaintext); j < m; j++)
     {
         // If uppercase alphabetical character, use handleUppercase function
         if (strPlaintext[j] >= 65 && strPlaintext[j] <= 90)
         {
-            handleUppercase(key, strPlaintext, j);
+            handleCase(64, 90, key, strPlaintext, j);
         }
         // If lowercase alphabetical character, use handleUppercase function
-        else if (strPlaintext[j] >= 97 && strPlaintext[j] <= 121)
+        else if (strPlaintext[j] >= 97 && strPlaintext[j] <= 122)
         {
-            handleLowercase(key, strPlaintext, j);
+            handleCase(96, 122, key, strPlaintext, j);
         }
     }
     printf("\n");
 }
 
-// Function to handle uppercase characters
-void handleUppercase(int key, string strPlaintext, int j)
+void handleCase(int lowerbound, int upperbound, int key, string strPlaintext, int j)
 {
     int intPlaintext = (int)strPlaintext[j];
     int intCiphertext = intPlaintext + key;
-    // If intPlaintext + key = character ASCII code past uppercase Z, start from uppercase A
-    if (intCiphertext > 90)
+    // If intPlaintext + key = character ASCII code past z, start from a
+    if (intCiphertext > upperbound)
     {
-        intCiphertext = 64 + (90 - intPlaintext) + key;
-        printf("%c", (char)intCiphertext);
-    }
-    // Else use intPlaintext + key
-    else
-    {
-        printf("%c", (char)intCiphertext);
-    }
-}
-
-// Function to handle lowercase characters
-void handleLowercase(int key, string strPlaintext, int j)
-{
-    int intPlaintext = (int)strPlaintext[j];
-    int intCiphertext = intPlaintext + key;
-    // If intPlaintext + key = character ASCII code past lowercase z, start from lowercase a
-    if (intCiphertext > 122)
-    {
-        intCiphertext = 97 + (122 - intPlaintext) + key;
+        intCiphertext = lowerbound + (upperbound - intPlaintext) + key;
         printf("%c", (char)intCiphertext);
     }
     // Else use intPlaintext + key
