a9c8d3aa9981d3c35bfdb08c340770726c0942f5
Abelard Bautista <abelardbautista@gmail.com>
2020-10-15 23:21:48 -0700

    Handles encryption dependent on capitalization.

--- w2/caesar/caesar.c
+++ w2/caesar/caesar.c
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+void handleUppercase(int key, string strPlaintext, int j);
+void handleLowercase(int key, string strPlaintext, int j);
+
 int main(int argc, string argv[])
 {
     // Produce error if not two command-line arguments
@@ -24,18 +27,54 @@ int main(int argc, string argv[])
     int key = atoi(argv[1]) % 26;
     printf("Key = %i\n", key);
     string strPlaintext = get_string("plaintext: ");
+
     for (int j = 0, m = strlen(strPlaintext); j < m; j++)
     {
-        int intCiphertext = (int)strPlaintext[j] + key;
-        if (intCiphertext > 122)
+        if (strPlaintext[j] >= 65 && strPlaintext[j] <= 90)
         {
-            intCiphertext = 96 + (122 - (int)strPlaintext[j]) + key;
-            printf("%c", (char)intCiphertext);
+            handleUppercase(key, strPlaintext, j);
         }
-        else
+        else if (strPlaintext[j] >= 97 && strPlaintext[j] <= 121)
         {
-            printf("%c", (char)intCiphertext);
+            handleLowercase(key, strPlaintext, j);
         }
     }
+
     printf("\n");
+}
+
+void handleUppercase(int key, string strPlaintext, int j)
+{
+    // for (int k = 0, p = strlen(strPlaintext); k < p; k++)
+    // {
+    int intPlaintext = (int)strPlaintext[j];
+    int intCiphertext = intPlaintext + key;
+    if (intCiphertext > 90)
+    {
+        intCiphertext = 64 + (90 - intPlaintext) + key;
+        printf("%c", (char)intCiphertext);
+    }
+    else
+    {
+        printf("%c", (char)intCiphertext);
+    }
+    // }
+}
+
+void handleLowercase(int key, string strPlaintext, int j)
+{
+    // for (int l = 0, q = strlen(strPlaintext); l < q; l++)
+    // {
+    int intPlaintext = (int)strPlaintext[j];
+    int intCiphertext = intPlaintext + key;
+    if (intCiphertext > 122)
+    {
+        intCiphertext = 97 + (122 - intPlaintext) + key;
+        printf("%c", (char)intCiphertext);
+    }
+    else
+    {
+        printf("%c", (char)intCiphertext);
+    }
+    // }
 }
