fb607dac2001a11274a8b9a76ccd3460c03c68c8
Abelard Bautista <abelardbautista@gmail.com>
2020-10-15 23:36:41 -0700

    Cleaned code. Added comments.

--- w2/caesar/caesar.c
+++ w2/caesar/caesar.c
@@ -24,57 +24,59 @@ int main(int argc, string argv[])
             return 1;
         }
     }
+    // Encryption will only change within alphabetical characters
     int key = atoi(argv[1]) % 26;
-    printf("Key = %i\n", key);
+    // Ask for user input for plaintext
     string strPlaintext = get_string("plaintext: ");
-
+    // Check each character from the beginning to the end of the string length
     for (int j = 0, m = strlen(strPlaintext); j < m; j++)
     {
+        // If uppercase alphabetical character, use handleUppercase function
         if (strPlaintext[j] >= 65 && strPlaintext[j] <= 90)
         {
             handleUppercase(key, strPlaintext, j);
         }
+        // If lowercase alphabetical character, use handleUppercase function
         else if (strPlaintext[j] >= 97 && strPlaintext[j] <= 121)
         {
             handleLowercase(key, strPlaintext, j);
         }
     }
-
     printf("\n");
 }
 
+// Function to handle uppercase characters
 void handleUppercase(int key, string strPlaintext, int j)
 {
-    // for (int k = 0, p = strlen(strPlaintext); k < p; k++)
-    // {
     int intPlaintext = (int)strPlaintext[j];
     int intCiphertext = intPlaintext + key;
+    // If intPlaintext + key = character ASCII code past uppercase Z, start from uppercase A
     if (intCiphertext > 90)
     {
         intCiphertext = 64 + (90 - intPlaintext) + key;
         printf("%c", (char)intCiphertext);
     }
+    // Else use intPlaintext + key
     else
     {
         printf("%c", (char)intCiphertext);
     }
-    // }
 }
 
+// Function to handle lowercase characters
 void handleLowercase(int key, string strPlaintext, int j)
 {
-    // for (int l = 0, q = strlen(strPlaintext); l < q; l++)
-    // {
     int intPlaintext = (int)strPlaintext[j];
     int intCiphertext = intPlaintext + key;
+    // If intPlaintext + key = character ASCII code past lowercase z, start from lowercase a
     if (intCiphertext > 122)
     {
         intCiphertext = 97 + (122 - intPlaintext) + key;
         printf("%c", (char)intCiphertext);
     }
+    // Else use intPlaintext + key
     else
     {
         printf("%c", (char)intCiphertext);
     }
-    // }
 }
