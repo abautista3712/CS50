1994ea7303e483d52b2cca4653e6be8712d9b8cb
Abelard Bautista <abelardbautista@gmail.com>
2020-10-16 21:10:55 -0700

    Added specifications for command-line argument input.

--- w2/substitution/substitution.c
+++ w2/substitution/substitution.c
@@ -1,7 +1,27 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    printf("Test\n");
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if (!isalpha(argv[1][i]))
+        {
+            printf("Usage: ./substitution key\n");
+            return 1;
+        }
+        else if (n != 26)
+        {
+            printf("Key must contain 26 characters.\n");
+            return 1;
+        }
+    }
+    printf("End of Test\n");
 }
