b6a50b27bcce05c978e914b3620db24ad19d6629
Abelard Bautista <abelardbautista@gmail.com>
2020-10-17 22:02:10 -0700

    Plaintext is encrypted to ciphertext using key.

--- w2/substitution/substitution.c
+++ w2/substitution/substitution.c
@@ -3,62 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int caseInsensitive(int intSearchTerm);
 void handleCipher(string plaintext, string key);
-
-int main(int argc, string argv[])
-{
-    if (argc != 2)
-    {
-        printf("Usage: ./substitution key\n");
-        return 1;
-    }
-    for (int i = 0, n = strlen(argv[1]); i < n; i++)
-    {
-        if (!isalpha(argv[1][i]))
-        {
-            printf("Usage: ./substitution key\n");
-            return 1;
-        }
-        else if (n != 26)
-        {
-            printf("Key must contain 26 characters.\n");
-            return 1;
-        }
-    }
-
-    string key = argv[1];
-    string plaintext = get_string("plaintext: ");
-    handleCipher(plaintext, key);
-}
-
-int caseInsensitive(int intSearchTerm)
-{
-    if (intSearchTerm >= 97 && intSearchTerm <= 122)
-    {
-        intSearchTerm -= 97;
-    }
-    else if (intSearchTerm >= 65 && intSearchTerm <= 90)
-    {
-        intSearchTerm -= 65;
-    }
-    return intSearchTerm;
-}
-
-// int intKeyPosition (string key)
-// {
-//     for (int j = 0; j < 26; j++)
-//     {
-//         int keyPosition = (int) key[j]
-//     }
-// }#include <cs50.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-
 int caseInsensitive(int intSearchTerm);
-void calculateShift(string key);
-void handleCipher(string plaintext);
+int encryptPlaintext(int intPlaintext, string key);
 
 int main(int argc, string argv[])
 {
@@ -80,44 +27,10 @@ int main(int argc, string argv[])
             return 1;
         }
     }
-
     string key = argv[1];
     string plaintext = get_string("plaintext: ");
-    calculateShift(key);
-    handleCipher(plaintext);
-}
-
-int caseInsensitive(int intSearchTerm)
-{
-    if (intSearchTerm >= 97 && intSearchTerm <= 122)
-    {
-        intSearchTerm -= 97;
-    }
-    else if (intSearchTerm >= 65 && intSearchTerm <= 90)
-    {
-        intSearchTerm -= 65;
-    }
-    return intSearchTerm;
-}
-
-void calculateShift(string key)
-{
-    for (int j = 0; j < 26; j++)
-    {
-        int intKeyPosition = (int)key[j];
-        intKeyPosition = caseInsensitive(intKeyPosition);
-        printf("intKeyPosition = %i\n", intKeyPosition);
-    }
-}
-
-void handleCipher(string plaintext)
-{
-    for (int j = 0, o = strlen(plaintext); j < o; j++)
-    {
-        int intPlaintext = (int)plaintext[j];
-        intPlaintext = caseInsensitive(intPlaintext);
-        printf("intPlaintext = %i\n", intPlaintext);
-    }
+    printf("ciphertext: ");
+    handleCipher(plaintext, key);
 }
 
 void handleCipher(string plaintext, string key)
@@ -125,7 +38,28 @@ void handleCipher(string plaintext, string key)
     for (int j = 0, o = strlen(plaintext); j < o; j++)
     {
         int intPlaintext = (int)plaintext[j];
-        intPlaintext = caseInsensitive(intPlaintext);
-        printf("%i\n", intPlaintext);
+        int intCiphertext = encryptPlaintext(intPlaintext, key);
+        printf("%c", (char)intCiphertext);
     }
+    printf("\n");
+}
+
+int caseInsensitive(int intSearchTerm)
+{
+    if (intSearchTerm >= 97 && intSearchTerm <= 122)
+    {
+        intSearchTerm -= 97;
+    }
+    else if (intSearchTerm >= 65 && intSearchTerm <= 90)
+    {
+        intSearchTerm -= 65;
+    }
+    return intSearchTerm;
+}
+
+int encryptPlaintext(int intPlaintext, string key)
+{
+    int ci_intPlaintext = caseInsensitive(intPlaintext);
+    int ci_intCiphertext = key[ci_intPlaintext];
+    return ci_intCiphertext;
 }
