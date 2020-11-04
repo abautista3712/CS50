2724b951d1131a40f9186b50b63df2ecc610dbec
Abelard Bautista <abelardbautista@gmail.com>
2020-11-03 23:22:03 -0800

    Added stdio.h and printf for easier visualization.

--- w4/filter/helpers.c
+++ w4/filter/helpers.c
@@ -1,10 +1,12 @@
 #include "helpers.h"
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < width; i++)
     {
+        printf("%i\n", image[100][i].rgbtRed);
         image[100][i].rgbtRed = 255;
         image[100][i].rgbtGreen = 255;
         image[100][i].rgbtBlue = 255;
