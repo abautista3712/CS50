3281ed83cfdc686b8192234539e2e7acfa5fb5ff
Abelard Bautista <abelardbautista@gmail.com>
2020-12-05 03:13:12 -0800

    Added input query and countDigits def.

--- w6/credit.py
+++ w6/credit.py
@@ -1,2 +1,17 @@
 from cs50 import get_int
 from cs50 import get_string
+
+while True:
+    creditNum = get_int("Input CC number to see if valid: ")
+
+    if creditNum > 0:
+        counterDigits = countDigits(creditNum)
+
+def countDigits(numberToCount):
+    counterDigits = 0
+
+    while numberToCount != 0:
+        numberToCount = numberToCount / 10
+        counterDigits += 1
+
+    return counterDigits
