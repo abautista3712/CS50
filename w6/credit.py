f3493787ae02bfa060e63e112c618ab05abf6aa9
Abelard Bautista <abelardbautista@gmail.com>
2020-12-05 03:30:11 -0800

    Wrote verifyCC function.

--- w6/credit.py
+++ w6/credit.py
@@ -1,12 +1,18 @@
 from cs50 import get_int
 from cs50 import get_string
+from math import floor
 
 while True:
     creditNum = get_int("Input CC number to see if valid: ")
 
     if creditNum > 0:
+        # Check number of digits
         counterDigits = countDigits(creditNum)
+        # Cards with 13, 15, 16 digits are VALID and will undergo further verification
+        if counterDigits == 13 or counterDigits == 15 or counterDigits == 16:
+            verifyCC(creditNum, counterDigits)
 
+# Function to count credit card digits
 def countDigits(numberToCount):
     counterDigits = 0
 
@@ -15,3 +21,25 @@ def countDigits(numberToCount):
         counterDigits += 1
 
     return counterDigits
+
+# Function to verify CC and print which type of card
+def verifyCC(ccNum, ccNumDigits):
+    if luhnCheck(ccNum, ccNumDigits) == false:
+        print("INVALID")
+    else:
+        firstTwoCheck = floor(ccNum / 1 * (10 ** ccNumDigits - 2))
+
+        # AMEX Verification via First Two Numbers
+        if firstTwoCheck == 34 or firstTwoCheck == 37:
+            print("AMEX")
+
+        # MASTERCARD Verification via First Two Numbers
+        elif firstTwoCheck == 51 or firstTwoCheck == 52 or firstTwoCheck == 54 or firstTwoCheck == 55:
+            print("MASTERCARD")
+
+        # VISA via First Number
+        elif floor(ccNum / (1 * (10 ** ccNumDigits - 1)) == 4)
+            print("VISA")
+
+        else:
+            print("INVALID")
