19060cc632807cbfe6258b8450821a5880c6ca7a
Abelard Bautista <abelardbautista@gmail.com>
2020-12-05 00:31:28 -0800

    Changed periods to spaces in j loop.

--- w6/marioless.py
+++ w6/marioless.py
@@ -7,7 +7,7 @@ while True:
         for i in range(height):
 
             for j in range(height - 1, i, -1):
-                print(".", end="")
+                print(" ", end="")
             print("#", end="")
 
             for k in range(i):
