1f69dc48878af3e9e4cc3fea480a141e59925a0e
Abelard Bautista <abelardbautista@gmail.com>
2020-12-06 20:30:25 -0800

    Started countWords function. Tested isalnum().

--- w6/readability.py
+++ w6/readability.py
@@ -1,5 +1,7 @@
-inputText = input("Text: ")
-letters = countLetters(inputText)
+def main():
+    inputText = input("Text: ")
+    letters = countLetters(inputText)
+    words = countWords(inputText)
 
 def countLetters(inputText):
     letters = 0
@@ -7,3 +9,13 @@ def countLetters(inputText):
     for i in range(n):
         letters += 1
     return letters
+
+def countWords(inputText):
+    words = 0
+    if str(inputText[:1]).isalnum():
+        words = 1
+    # n = len(inputText)
+    # for i in range(n):
+    #     if
+
+# main()
