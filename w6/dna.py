ea7bee28bd0a1923ddcae20a3481adae664db4db
Abelard Bautista <abelardbautista@gmail.com>
2020-12-07 00:20:10 -0800

    Simplified open code.

--- w6/dna.py
+++ w6/dna.py
@@ -6,21 +6,23 @@ if len(argv) != 3:
     exit(1)
 
 with open(f"./{argv[1]}") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Columns Titles: {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f"Row {line_count}: ", end="")
-            print(f'{", ".join(row)}')
-            line_count += 1
-    print(f"Processed {line_count} lines.")
+    print(csv_file.read())
+    # csv_reader = csv.reader(csv_file, delimiter=",")
+    # line_count = 0
+    # for row in csv_reader:
+    #     if line_count == 0:
+    #         print(f'Columns Titles: {", ".join(row)}')
+    #         line_count += 1
+    #     else:
+    #         print(f"Row {line_count}: ", end="")
+    #         print(f'{", ".join(row)}')
+    #         line_count += 1
+    # print(f"Processed {line_count} lines.")
 
 with open(f"./{argv[2]}") as sequence_file:
-    sequence_reader = csv.reader(sequence_file, delimiter=",")
-    for row in sequence_reader:
-        print(f'DNA Sequence: {", ".join(row)}')
+    print(sequence_file.read())
+    # for row in sequence_reader:
+    #     print(f'DNA Sequence: {", ".join(row)}')
+    #     print(f"{len(sequence_reader)}")
 
 exit(0)
