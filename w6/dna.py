19d58fda83d5a2c38b4ad98640bee6af9209766f
Abelard Bautista <abelardbautista@gmail.com>
2020-12-07 00:01:45 -0800

    Handle dynamic input of command line arguements.

--- w6/dna.py
+++ w6/dna.py
@@ -1,11 +1,11 @@
 import csv
 from sys import argv, exit
 
-if len(argv) != 2:
+if len(argv) != 3:
     print("Missing command-line argument")
     exit(1)
 
-with open("./databases/small.csv") as csv_file:
+with open(f"./{argv[1]}") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 0
     for row in csv_reader:
@@ -18,7 +18,7 @@ with open("./databases/small.csv") as csv_file:
             line_count += 1
     print(f"Processed {line_count} lines.")
 
-with open("./sequences/1.txt") as sequence_file:
+with open(f"./{argv[2]}") as sequence_file:
     sequence_reader = csv.reader(sequence_file, delimiter=",")
     for row in sequence_reader:
         print(f'DNA Sequence: {", ".join(row)}')
