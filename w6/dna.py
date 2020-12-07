56840c0bc299c074f3a10e3248a7f12019a483ba
Abelard Bautista <abelardbautista@gmail.com>
2020-12-06 23:55:30 -0800

    Open csv and seqence and print to console.

--- w6/dna.py
+++ w6/dna.py
@@ -1,7 +1,26 @@
+import csv
 from sys import argv, exit
 
 if len(argv) != 2:
     print("Missing command-line argument")
     exit(1)
-print(f"Test, {argv[1]}")
+
+with open("./databases/small.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Columns Titles: {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f"Row {line_count}: ", end="")
+            print(f'{", ".join(row)}')
+            line_count += 1
+    print(f"Processed {line_count} lines.")
+
+with open("./sequences/1.txt") as sequence_file:
+    sequence_reader = csv.reader(sequence_file, delimiter=",")
+    for row in sequence_reader:
+        print(f'DNA Sequence: {", ".join(row)}')
+
 exit(0)
