3621f7dae734569236be2fe9b3a301f11807dc53
Abelard Bautista <abelardbautista@gmail.com>
2020-12-07 23:28:31 -0800

    Refined STR matching code and added code to keep track of max consecutive matches.

--- w6/dna.py
+++ w6/dna.py
@@ -5,6 +5,7 @@ if len(argv) != 3:
     print("Missing command-line argument")
     exit(1)
 
+# Open CSV file
 with open(f"./{argv[1]}") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 0
@@ -14,28 +15,35 @@ with open(f"./{argv[1]}") as csv_file:
             str = csv_row[1]
             str_length = len(str)
 
+            print(str)
+
             # Open DNA Sequence txt file
             with open(f"./{argv[2]}") as sequence_file:
                 dna = sequence_file.read()
 
                 # Compare STR to DNA Sequence
-                # str_match = 0
-
+                # Declare empty dictionary and max_consecutive variable
                 str_match = {}
+                max_consecutive = 0
 
+                # Loop through every DNA base
                 for i in range(len(dna)):
-                    if dna[i:(len(str) + i)] == str:
-                        str_match[i] = 1
-                    else:
-                        str_match[i] = 0
-                # print(f"{str_match}")
+                    # Initialize default value of str_match[i] to 0
+                    str_match[i] = 0
 
-                thisdict = {
-                    "brand": "Ford",
-                    "model": "Mustang",
-                    "year": 1964
-                }
+                    # Check STR against DNA sequence and count consecutive occurances
+                    if dna[i:(len(str) + i)] == str:
+                        if str_match[i - len(str)] > 0:
+                            str_match[i] = str_match[i - len(str)] + 1
+                        else:
+                            str_match[i] = 1
+
+                    # Keep track of max number of consecutive STR matches
+                    if str_match[i] > max_consecutive:
+                        max_consecutive = str_match[i]
+
                 print(str_match)
+                print(f"max_consecutive = {max_consecutive}")
             # for i in range(len(Full DNA Sequence)):
             #     if STR Segment == DNA Sequence Segment:
             #         STR Count += 1
