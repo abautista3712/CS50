e37fda8a7d05e69b8817e42cccb41b009ffe6029
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 05:35:37 -0800

    Consolidated db import.

--- w7/houses/roster.py
+++ w7/houses/roster.py
@@ -10,19 +10,18 @@ if len(argv) != 2:
 db = SQL("sqlite:///students.db")
 
 # Assign data from db to variables
-db_name = db.execute(f"SELECT first, middle, last FROM students WHERE house = '{argv[1]}' ORDER BY last, first ASC")
-db_birth = db.execute(f"SELECT birth FROM students WHERE house = '{argv[1]}'")
+db_info = db.execute(f"SELECT first, middle, last, birth FROM students WHERE house = '{argv[1]}' ORDER BY last, first ASC")
 
 # Handle pringting to console
 # Assign variable for length of db query
-student_length = len(db_name)
+student_length = len(db_info)
 
 for i in range(student_length):
     # Handle Special Case: Null value of middle name
-    if db_name[i]['middle'] == None:
-        db_name[i]['middle'] = " "
+    if db_info[i]['middle'] == None:
+        db_info[i]['middle'] = " "
     else:
-        db_name[i]['middle'] = " " + db_name[i]['middle'] + " "
+        db_info[i]['middle'] = " " + db_info[i]['middle'] + " "
 
     # Print data to console
-    print(f"{db_name[i]['FIRST']}{db_name[i]['middle']}{db_name[i]['LAST']}, born {db_birth[i]['birth']}")
+    print(f"{db_info[i]['FIRST']}{db_info[i]['middle']}{db_info[i]['LAST']}, born {db_info[i]['birth']}")
