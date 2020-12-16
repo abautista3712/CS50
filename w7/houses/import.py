b3d23749a140e255060471660c3f88892ffc3686
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 03:08:07 -0800

    Name inserted into table. Print to console.

--- w7/houses/import.py
+++ w7/houses/import.py
@@ -21,7 +21,30 @@ with open(f"{argv[1]}", "r") as csv_file:
     line_count = 0
 
     for csv_row in csv_reader:
-        print(f"{line_count}")
-        name_split = csv_row[0].split()
-        print(f"{name_split}")
+        if line_count == 0:
+            line_count += 1
+        else:
+            # Split first/middle/last name by spaces
+            name_split = csv_row[0].split()
+
+            print(f"{name_split}")
+            if len(name_split) == 2:
+                print("name_split == 2")
+                first = name_split[0]
+                middle = None
+                last = name_split[1]
+            if len(name_split) == 3:
+                print("name_split == 3")
+                first = name_split[0]
+                middle = name_split[1]
+                last = name_split[2]
+
+            print(f"{len(name_split)}")
+
+            db.execute("INSERT INTO students (id, first, middle, last) VALUES(?, ?, ?, ?)", line_count, first, middle, last)
+
+            db.execute("SELECT * FROM students")
+
+        print(f"Line count: {line_count}")
         line_count += 1
+
