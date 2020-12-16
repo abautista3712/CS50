2f8403e691ba24634823b2c78b3ed6d713dc0837
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 02:20:26 -0800

    Open CSV file and loop through rows. Print name and line_count.

--- w7/houses/import.py
+++ w7/houses/import.py
@@ -13,6 +13,14 @@ db = cs50.SQL("sqlite:///students.db")
 # Create tables
 db.execute("CREATE TABLE students (id INT, first TEXT, middle TEXT, last TEXT, PRIMARY KEY(id))")
 
-# # Open CSV file
-# with open(f"./{argv[1]}") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=",")
+# Open CSV file
+with open(f"{argv[1]}", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Variable to count lines
+    line_count = 0
+
+    for csv_row in csv_reader:
+        print(f"{csv_row[0]}")
+        print(f"{line_count}")
+        line_count += 1
