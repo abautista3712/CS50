7ee6cdd9a3b3a15ff578cfb9f7247c9e630f9dba
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 03:32:41 -0800

    Consolidate insertion into one line. Save db into variable and print.

--- w7/houses/import.py
+++ w7/houses/import.py
@@ -11,7 +11,7 @@ open(f"students.db", "w").close()
 db = cs50.SQL("sqlite:///students.db")
 
 # Create tables
-db.execute("CREATE TABLE students (id INT, first TEXT, middle TEXT, last TEXT, PRIMARY KEY(id))")
+db.execute("CREATE TABLE students (id INT, first TEXT, middle TEXT, last TEXT, birth INT, PRIMARY KEY(id))")
 
 # Open CSV file
 with open(f"{argv[1]}", "r") as csv_file:
@@ -27,24 +27,23 @@ with open(f"{argv[1]}", "r") as csv_file:
             # Split first/middle/last name by spaces
             name_split = csv_row[0].split()
 
-            print(f"{name_split}")
             if len(name_split) == 2:
-                print("name_split == 2")
                 first = name_split[0]
                 middle = None
                 last = name_split[1]
             if len(name_split) == 3:
-                print("name_split == 3")
                 first = name_split[0]
                 middle = name_split[1]
                 last = name_split[2]
 
-            print(f"{len(name_split)}")
+            # Insert names into SQL
+            db.execute("INSERT INTO students (id, first, middle, last, birth) VALUES(?, ?, ?, ?, ?)", line_count, first, middle, last, csv_row[2])
 
-            db.execute("INSERT INTO students (id, first, middle, last) VALUES(?, ?, ?, ?)", line_count, first, middle, last)
-
-            db.execute("SELECT * FROM students")
-
-        print(f"Line count: {line_count}")
+        # print(f"Line count: {line_count}")
         line_count += 1
+
+    db_info = db.execute("SELECT * FROM students")
+
+    print(f"{db_info}")
+
 
