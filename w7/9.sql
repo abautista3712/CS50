a7c044ccadb6755f8e11128d1bacd92e34c8e462
Abelard Bautista <abelardbautista@gmail.com>
2020-12-15 20:42:39 -0800

    Changed JOIN tables to linked WHERE IN clauses. Filtered by ID rather than by name.

--- w7/9.sql
+++ w7/9.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
-ORDER BY birth;
+SELECT people.id, people.name, people.birth FROM people
+WHERE people.id IN (SELECT DISTINCT stars.person_id FROM stars
+WHERE stars.movie_id IN (SELECT movies.id FrOM movies
+WHERE movies.year = 2004))
+ORDER BY people.birth;
