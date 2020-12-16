28261c396685fc638a56ea176158d13481310d73
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 04:35:30 -0800

    Connect to db and print raw data.

--- w7/houses/roster.py
+++ w7/houses/roster.py
@@ -1,7 +1,14 @@
-import cs50
+from cs50 import SQL
 import csv
 from sys import argv, exit
 
 if len(argv) != 2:
     print("Missing command-line argument")
     exit(1)
+
+# Connect to DB
+db = SQL("sqlite:///students.db")
+
+db_info = db.execute("SELECT * FROM students")
+
+print(f"{db_info}")
