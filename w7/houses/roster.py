7db068491cec70aac3586794b59159558f9848d9
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 05:24:28 -0800

    Print students per class.

--- w7/houses/roster.py
+++ w7/houses/roster.py
@@ -10,11 +10,12 @@ if len(argv) != 2:
 db = SQL("sqlite:///students.db")
 
 # Assign data from db to variables
-db_name = db.execute("SELECT first, middle, last FROM students ORDER BY last, first ASC")
-db_birth = db.execute("SELECT birth FROM students")
+db_name = db.execute(f"SELECT first, middle, last FROM students WHERE house = '{argv[1]}' ORDER BY last, first ASC")
+db_birth = db.execute(f"SELECT birth FROM students WHERE house = '{argv[1]}'")
 
-# Print to console
-student_length = len(db.execute("SELECT * FROM students"))
+# Handle pringting to console
+# Assign variable for length of db query
+student_length = len(db_name)
 
 for i in range(student_length):
     # Handle Special Case: Null value of middle name
@@ -23,4 +24,5 @@ for i in range(student_length):
     else:
         db_name[i]['middle'] = " " + db_name[i]['middle'] + " "
 
+    # Print data to console
     print(f"{db_name[i]['FIRST']}{db_name[i]['middle']}{db_name[i]['LAST']}, born {db_birth[i]['birth']}")
