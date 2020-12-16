e340dd2d64b6deeb5c9548c972de8a2097598c7c
Abelard Bautista <abelardbautista@gmail.com>
2020-12-15 20:43:58 -0800

    Removed SELECT for id and birth as per specification.

--- w7/9.sql
+++ w7/9.sql
@@ -1,4 +1,4 @@
-SELECT people.id, people.name, people.birth FROM people
+SELECT people.name FROM people
 WHERE people.id IN (SELECT DISTINCT stars.person_id FROM stars
 WHERE stars.movie_id IN (SELECT movies.id FrOM movies
 WHERE movies.year = 2004))
