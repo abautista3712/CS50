66e329a147328bc9c6ffd1ed052e5f5304c929d8
Abelard Bautista <abelardbautista@gmail.com>
2020-12-16 01:16:21 -0800

    Fixed command-line argument value. Added tables.

--- w7/houses/import.py
+++ w7/houses/import.py
@@ -2,13 +2,16 @@ import cs50
 import csv
 from sys import argv, exit
 
-if len(argv) != 3:
+if len(argv) != 2:
     print("Missing command-line argument")
     exit(1)
 
 # Create database
 open(f"students.db", "w").close()
 db = cs50.SQL("sqlite:///students.db")
+
+# Create tables
+db.execute("CREATE TABLE students (id INT, first TEXT, middle TEXT, last TEXT, PRIMARY KEY(id))")
 
 # # Open CSV file
 # with open(f"./{argv[1]}") as csv_file:
