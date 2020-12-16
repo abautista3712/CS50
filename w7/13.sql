d72ebe20fa7c4c09f1f23eb44299d8e7f14d7db9
Abelard Bautista <abelardbautista@gmail.com>
2020-12-15 23:31:22 -0800

    Added/fixed comments.

--- w7/13.sql
+++ w7/13.sql
@@ -1,13 +1,14 @@
+-- Select people.name
 SELECT people.name FROM people
 WHERE people.id IN (
 SELECT stars.person_id FROM stars
 WHERE stars.movie_id IN (
--- Select movies.id
+-- From selection of movies.id
 SELECT movies.id FROM movies
 WHERE movies.id IN (
 SELECT stars.movie_id FROM stars
 WHERE person_id IN (
--- Select people.id for Kevin Bacon born in 1958
+-- From selection where people.id is Kevin Bacon born in 1958
 SELECT people.id FROM people
 WHERE people.name = "Kevin Bacon"
 AND people.birth = 1958))));
