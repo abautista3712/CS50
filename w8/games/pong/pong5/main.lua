6d3a4a97a43ac70770dad80f157f83f23eb3920b
Abelard Bautista <abelardbautista@gmail.com>
2020-12-19 23:48:37 -0800

    Imported class from distribution code.

---
+++ w8/games/pong/pong5/main.lua
@@ -0,0 +1,109 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
+
+push = require 'push'
+
+function love.load()
+    math.randomseed(os.time())
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = false
+    })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 40
+
+    function initializeBall()
+        ballX = VIRTUAL_WIDTH / 2 - 2
+        ballY = VIRTUAL_HEIGHT / 2 - 2
+
+        ballDX = math.random(2) == 1 and -100 or 100
+        ballDY = math.random(-50, 50)
+    end
+
+    initializeBall()
+
+    gameState = 'start'
+end
+
+function love.update(dt)
+    -- Player 1 (Left Paddle) Movement
+    if love.keyboard.isDown('w') then
+        player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('s') then
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+    end
+
+    -- Player 2 (Right Paddle) Movement
+    if love.keyboard.isDown('up') then
+        player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
+end
+
+function love.keypressed(key)
+    -- Escape game using ESC key
+    if key == 'escape' then
+        love.event.quit()
+    -- Change game state from 'start' to 'play'
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        elseif gameState == 'play' then
+            gameState = 'start'
+            initializeBall()
+        end
+    end
+end
+
+function love.draw()
+    push:apply('start')
+
+    -- Render Background Color
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+    -- Render Welcome Message
+    love.graphics.setFont(smallFont)
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'play' then
+        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
+    -- Render Score
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    
+    -- Render First Paddle (Left Side)
+    love.graphics.rectangle('fill', 5, player1Y, 5, 20)
+    
+    -- Render Second Paddle (Right Side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    
+    -- Render Ball (Center)
+    love.graphics.rectangle('fill', ballX, ballY, 5, 5)
+    
+    push:apply('end')
+end
