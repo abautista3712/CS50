708067fd86206c569e852b807343e869b852eaed
Abelard Bautista <abelardbautista@gmail.com>
2020-12-19 17:28:58 -0800

    Import and set font.ttf.

---
+++ w8/games/pong/pong2/main.lua
@@ -0,0 +1,39 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+function love.load()
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = false
+    })
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
+    push:apply('start')
+
+    love.graphics.printf(
+        'Hello Pong!', 
+        0, 
+        VIRTUAL_HEIGHT / 2 - 6, 
+        VIRTUAL_WIDTH, 
+        'center')
+
+    push:apply('end')
+end
