4ecd5e33c45a5b667cad9ad798d75221851a36c8
Abelard Bautista <abelardbautista@gmail.com>
2020-12-19 23:24:43 -0800

    Added random ball velocity at start.

--- w8/games/pong/pong4/main.lua
+++ w8/games/pong/pong4/main.lua
@@ -9,11 +9,19 @@ PADDLE_SPEED = 200
 push = require 'push'
 
 function love.load()
+    math.randomseed(os.time())
+
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
     
     scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = false
+    })
 
     player1Score = 0
     player2Score = 0
@@ -21,11 +29,13 @@ function love.load()
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 40
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        vsync = true,
-        resizable = false
-    })
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    ballDX = math.random(2) == 1 and -100 or 100
+    ballDY = math.random(-50, 50)
+
+    gameState = 'play'
 end
 
 function love.update(dt)
@@ -42,9 +52,15 @@ function love.update(dt)
     elseif love.keyboard.isDown('down') then
         player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
     end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
+    end
 end
 
 function love.keypressed(key)
+    -- Escape game using ESC key
     if key == 'escape' then
         love.event.quit()
     end
@@ -56,15 +72,6 @@ function love.draw()
     -- Render Background Color
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
-    -- Render Ball (Center)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
-
-    -- Render First Paddle (Left Side)
-    love.graphics.rectangle('fill', 5, player1Y, 5, 20)
-
-    -- Render Second Paddle (Right Side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-
     -- Render Welcome Message
     love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
@@ -73,6 +80,15 @@ function love.draw()
     love.graphics.setFont(scoreFont)
     love.graphics.print(player1Score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
     love.graphics.print(player2Score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
-
+    
+    -- Render First Paddle (Left Side)
+    love.graphics.rectangle('fill', 5, player1Y, 5, 20)
+    
+    -- Render Second Paddle (Right Side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    
+    -- Render Ball (Center)
+    love.graphics.rectangle('fill', ballX, ballY, 5, 5)
+    
     push:apply('end')
 end
