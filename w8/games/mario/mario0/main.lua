94a2b3465d85eb9759366f59eccae4d0ba8ab2bc
Abelard Bautista <abelardbautista@gmail.com>
2020-12-23 21:57:41 -0800

    Initialized development environment.

---
+++ w8/games/mario/mario0/main.lua
@@ -0,0 +1,33 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+Class = require 'class'
+push = require 'push'
+
+require 'Map'
+
+function love.load()
+    map = Map()
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizeable = false,
+        vsync = true
+    })
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    push:apply('start')
+    love.graphics.clear(108/255, 140/255, 255/255, 255/255)
+    love.graphics.print("Hello, world!")
+    map:render()
+    push:apply('end')
+end
