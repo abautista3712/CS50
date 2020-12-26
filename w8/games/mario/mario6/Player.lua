572e3013595eaceb4c1d5cf4f4b22ffee5c2ff81
Abelard Bautista <abelardbautista@gmail.com>
2020-12-25 18:58:54 -0800

    Reflect movement animation when turning avatar.

--- w8/games/mario/mario6/Player.lua
+++ w8/games/mario/mario6/Player.lua
@@ -15,6 +15,7 @@ function Player:init(map)
     self.frames = generateQuads(self.texture, 16, 20)
 
     self.state = 'idle'    
+    self.direction = 'right'
 
     self.animations = {
         ['idle'] = Animation {
@@ -41,9 +42,11 @@ function Player:init(map)
             if love.keyboard.isDown('a') then
                 self.x = self.x - MOVE_SPEED * dt
                 self.animation = self.animations['walking']
+                self.direction = 'left'
             elseif love.keyboard.isDown('d') then
                 self.x = self.x + MOVE_SPEED * dt
                 self.animation = self.animations['walking']
+                self.direction = 'right'
             else
                 self.animation = self.animations['idle']
             end
@@ -52,9 +55,11 @@ function Player:init(map)
             if love.keyboard.isDown('a') then
                 self.x = self.x - MOVE_SPEED * dt
                 self.animation = self.animations['walking']
+                self.direction = 'left'
             elseif love.keyboard.isDown('d') then
                 self.x = self.x + MOVE_SPEED * dt
                 self.animation = self.animations['walking']
+                self.direction = 'right'
             else
                 self.animation = self.animations['idle']
             end
@@ -68,5 +73,17 @@ function Player:update(dt)
 end
 
 function Player:render()
-    love.graphics.draw(self.texture, self.animation:getCurrentFrame(), self.x, self.y)
+
+    local scaleX
+
+    if self.direction == 'right' then
+        scaleX = 1
+    else
+        scaleX = -1
+    end
+
+    love.graphics.draw(self.texture, self.animation:getCurrentFrame(), 
+        math.floor(self.x + self.width / 2), math.floor(self.y + self.height / 2),
+        0, scaleX, 1,
+        self.width / 2, self.height / 2)
 end
