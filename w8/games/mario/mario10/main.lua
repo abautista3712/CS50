c55210e837da4a2ea729f321e5d78999104ebee0
Abelard Bautista <abelardbautista@gmail.com>
2020-12-25 22:47:37 -0800

    Copied code from Mario9 as template.

---
+++ w8/games/mario/mario10/main.lua
@@ -0,0 +1,59 @@
+Class = require 'class'
+push = require 'push'
+
+require 'Map'
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1280
+
+math.randomseed(os.time())
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+map = Map()
+
+function love.load()
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizeable = true,
+        vsync = true
+    })
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+    map:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(108/255, 140/255, 255/255, 255/255)
+    
+    love.graphics.translate(math.floor(-map.camX + 0.5), math.floor(-map.camY + 0.5))
+    map:render()
+    
+    push:apply('end')
+end
