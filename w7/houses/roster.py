9263e76d2fa8f62d1c51b760b089c3271be11ef8
Abelard Bautista <abelardbautista@gmail.com>
2020-12-31 07:08:09 -0800

    Changed printing to console to accomodate check50 specification.

--- w7/houses/roster.py
+++ w7/houses/roster.py
@@ -13,15 +13,6 @@ db = SQL("sqlite:///students.db")
 db_info = db.execute(f"SELECT first, middle, last, birth FROM students WHERE house = '{argv[1]}' ORDER BY last, first ASC")
 
 # Handle pringting to console
-# Assign variable for length of db query
-student_length = len(db_info)
-
-for i in range(student_length):
-    # Handle Special Case: Null value of middle name
-    if db_info[i]['middle'] == None:
-        db_info[i]['middle'] = " "
-    else:
-        db_info[i]['middle'] = " " + db_info[i]['middle'] + " "
-
-    # Print data to console
-    print(f"{db_info[i]['FIRST']}{db_info[i]['middle']}{db_info[i]['LAST']}, born {db_info[i]['birth']}")
+for i in db_info:
+    first, middle, last, birth = i['first'], i['middle'], i['last'], i['birth']
+    print(f"{first} {middle + ' ' if middle else ''}{last}, born {birth}")
